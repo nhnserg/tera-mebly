8fee95ade1ade2962a9fe0f0f7721a7e03e6b12c
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-08-04 20:05:45 +0200

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: "export",
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true,
+  },
   transpilePackages: ["lucide-react"],
 };
 
