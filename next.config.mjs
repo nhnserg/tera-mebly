9c845e03f902c589a087e228741eb3f7e031543d
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-08-04 13:22:33 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  basePath: isProd ? "/nextjs-blog-deployment" : "",
+  // basePath: isProd ? "/nextjs-blog-deployment" : "",
   output: "export",
-  distDir: "dist",
-  images: {
-    unoptimized: true,
-  },
+  // distDir: "dist",
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
