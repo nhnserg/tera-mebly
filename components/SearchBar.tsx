dfecdcf185c6f718bbf10abe4fb20deed09c5548
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-08-04 13:10:02 +0200

    new
    commit static page

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -17,9 +17,9 @@ const SearchBar = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M21 21l-4.35-4.35M9 17a8 8 0 100-16 8 8 0 000 16z"
           ></path>
         </svg>
--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kBKjTWqZDSpx0rcl3/y563rJySrOGmyLqago678+FrE=\"\n}"
