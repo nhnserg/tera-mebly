d7bfc923d1bcb704949d9c629238096bbdc964f3
Serhii <124461383+nhnserg@users.noreply.github.com>
2024-08-07 14:34:48 +0200

    new

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   basePath: isProd ? "/tera-mebly" : "",
   output: "export",
+  reactStrictMode: true,
   distDir: "dist",
   images: {
     unoptimized: true,
